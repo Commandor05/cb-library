9d783a06d166ee3fb2e69fb4265f25278bd00926
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2024-07-02 21:28:18 +0300

    feat: add migration

---
+++ migrations/Version20240702181141.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240702181141 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE SEQUENCE author_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE author (id INT NOT NULL, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, second_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE book (id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, published_at DATE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_CBE5A331C53D045F ON book (image)');
+        $this->addSql('CREATE TABLE book_author (book_id INT NOT NULL, author_id INT NOT NULL, PRIMARY KEY(book_id, author_id))');
+        $this->addSql('CREATE INDEX IDX_9478D34516A2B381 ON book_author (book_id)');
+        $this->addSql('CREATE INDEX IDX_9478D345F675F31B ON book_author (author_id)');
+        $this->addSql('ALTER TABLE book_author ADD CONSTRAINT FK_9478D34516A2B381 FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE book_author ADD CONSTRAINT FK_9478D345F675F31B FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE author_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
+        $this->addSql('ALTER TABLE book_author DROP CONSTRAINT FK_9478D34516A2B381');
+        $this->addSql('ALTER TABLE book_author DROP CONSTRAINT FK_9478D345F675F31B');
+        $this->addSql('DROP TABLE author');
+        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE book_author');
+    }
+}
